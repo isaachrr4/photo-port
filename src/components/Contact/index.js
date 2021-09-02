644b9c174eadbe75417a3b93a8b6f8241617230c
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-09-02 15:46:24 -0400

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,30 +1,81 @@
-import React from 'React';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
-    return(
-        <section>
-        <h1>Contact me</h1>
-        <form id="contact-form">
-          // name input
-          // email input
-          // message text area
-        </form>
-        <div>
-      <label htmlFor="name">Name:</label>
-      <input type="text" name="name" />
-    </div>
-    <div>
-      <label htmlFor="email">Email address:</label>
-      <input type="email" name="email" />
-    </div>
-    <div>
-      <label htmlFor="message">Message:</label>
-      <textarea name="message" rows="5"  />
-    </div>
-    <button type="submit">Submit</button>
-      </section>
-    )
-    
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
     }
-    
-    export default ContactForm;
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
